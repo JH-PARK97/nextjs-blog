b2953c45b0cb2f8aaadacb069d831d3af9459386
Jun Hyung Park <jhpark@2online.io>
2025-02-26 17:56:32 +0900

    shadcn/ui setting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
+import { Button } from '@/components/ui/button';
 import { createClient } from '@/lib/supabase/serverClient';
 
 export default async function Instruments() {
   const supabase = await createClient();
   const { data: instruments } = await supabase.from('instruments').select();
 
-  return <pre>{JSON.stringify(instruments, null, 2)}</pre>;
+  return (
+    <>
+      <pre>{JSON.stringify(instruments, null, 2)}</pre>
+    </>
+  );
 }
